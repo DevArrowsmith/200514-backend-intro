76d0353e1b394b64606c35fbf6f1fca117f06bde
GummiCode <gummicodeblog@gmail.com>
2020-05-15 19:18:36 +0100

    Made the code modular; seperated the route that sends the message 'Hey there' into a distinct document from index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
+const helloRouter = require('./routes/hello');
 
 const app = express();
 
 app.listen(5555, () => console.log('app is istening on port 5555'));
 
-app.get('/', (req, res) => {
-  console.log(req);
-  res.status(200).send('Hello there!');
-});
-
+app.use('/hello', helloRouter);
